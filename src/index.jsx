f4cc3a9b4a2b85dcb8b44bc480e3169eced348d0
Ben Bradley <benjamin_bradley@brown.edu>
2025-08-17 18:58:58 -0400

    vite changes pt 2

--- src/index.jsx
+++ src/index.jsx
@@ -11,9 +11,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/bbradz.github.io/">
-      {" "}
-      {/* Add the basename prop here */}
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/bbradz.github.io/",
-  assetsInclude: ["**/*.heic", "**/*.HEIC", "**/*.JPG"], // Add this line to handle special file types
+  base: "/",
+  assetsInclude: ["**/*.heic", "**/*.HEIC", "**/*.JPG"],
 });
